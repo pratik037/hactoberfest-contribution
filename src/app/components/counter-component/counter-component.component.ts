f0785b86f91119db9a6d466e6592365872a7c26c
Pratik Singhal <singhalpratik037@gmail.com>
2022-10-01 20:30:48 +0530

    Added HomeComponent, CounterComponent, Routes, Angular Material. Changed the display title of the application

---
+++ src/app/components/counter-component/counter-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-component',
+  templateUrl: './counter-component.component.html',
+  styleUrls: ['./counter-component.component.css']
+})
+export class CounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
